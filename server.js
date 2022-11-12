ec68ac81b562e855ec9e200dd5bc76254510abc9
Deepak Yadav <deepakyadav101002@gmail.com>
2022-11-12 14:54:20 +0530

    new add

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.use((err, req, res, next) => {
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7483;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
